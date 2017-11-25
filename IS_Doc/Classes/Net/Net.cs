816c4745200d1473385e3464ce7ad7d303776d6a
Vladimir Bajenov195 <vsb95@ya.ru>
2017-11-25 18:30:23 +0600

    Добавлена форма входа

---
+++ IS_Doc/Classes/Net/Net.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS_Doc.Classes.Net
+{
+    /// <summary>
+    /// Класс реализующий работу с сервером
+    /// </summary>
+    public class Net
+    {
+        private static Net instance;
+
+        private Net() { }
+
+        public static Net GetInstance()
+        {
+            if (instance == null)
+                instance = new Net();
+            return instance;
+        }
+
+
+
+    }
+}
